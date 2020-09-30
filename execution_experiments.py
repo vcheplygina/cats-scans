3162227ca728b30f663cac73ed100a7f1fbb9d0a
irmavdbrandt <irma.vdbrandt@gmail.com>
2020-09-30 15:07:04 +0200

    change to local mongoDB

--- execution_experiments.py
+++ execution_experiments.py
@@ -10,8 +10,9 @@ import numpy as np
 # initialize experiment name. NOTE: this should be updated with every new experiment
 ex = Experiment('Resnet_pretrained=Imagenet_source=Isic')
 # create link with sacred MongoDB Atlas database
-ex.observers.append(MongoObserver(url="mongodb+srv://Irma:MIA-Bas-Veronika@cats-scans.eqbh3.mongodb.net/sacred"
-                                      "?retryWrites=true&w=majority"))
+ex.observers.append(MongoObserver())
+    # url="mongodb+srv://Irma:MIA-Bas-Veronika@cats-scans.eqbh3.mongodb.net/sacred"
+    #                                   "?retryWrites=true&w=majority"))
 
 
 @ex.config
