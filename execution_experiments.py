8e9f473d83e2a0cf534f3975754707043bed65fa
irmavdbrandt <irma.vdbrandt@gmail.com>
2020-10-11 23:24:44 +0200

    experiment settings imagenet+pcam+resnet

--- execution_experiments.py
+++ execution_experiments.py
@@ -44,7 +44,7 @@ def cfg():
     img_width = 96
     learning_rate = 0.00001
     batch_size = 128
-    epochs = 30
+    epochs = 20
     color = True
     dropout = 0.5
     model_choice = "resnet"
