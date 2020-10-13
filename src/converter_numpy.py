4ad57b90163b1cdacfdc9082f0539b908d1f225e
BasMulders12 <bas_mulders@hotmail.com>
2020-10-13 12:40:29 +0200

    Changed paths according to organized directories

--- src/converter_numpy.py
+++ src/converter_numpy.py
@@ -7,30 +7,30 @@ from os.path import isfile, join
 
 def get_train_images(dataset = 'ISIC2018', local_subset = 5):
     """Retrieve files from local depository and convert them to numpy arrays"""
+    begin_path = 'C:/Users/20169385/PycharmProjects/cats-scans/local_data/datasets'
     if dataset == 'ISIC2017':
-        path_to_dataset = '../datasets/ISIC2017/ISIC2017_Task3_Training_Input/'
+        path_to_dataset = '/ISIC2017/ISIC2017_Task3_Training_Input/'
     elif dataset == 'ISIC2018':
-        path_to_dataset = '../datasets/ISIC2018/ISIC2018_Task3_Training_Input/'
+        path_to_dataset = '/ISIC2018/ISIC2018_Task3_Training_Input/'
     elif dataset == 'chest_xray':
-        path_to_dataset = '../datasets/chest_xray/train/all/'
+        path_to_dataset = '/chest_xray/train/all/'
     elif dataset == 'stl-10':
-        path_to_dataset = '../datasets/stl_10/all/'
+        path_to_dataset = '/stl_10/all/'
     elif dataset == 'dtd':
-        path_to_dataset = '../datasets/dtd/all/'
+        path_to_dataset = '/dtd/all/'
     else:
         return None
 
-    dataset_filenames = [f for f in listdir(path_to_dataset) if
-                         isfile(join(path_to_dataset, f))][:local_subset]
+    dataset_filenames = [f for f in listdir(begin_path + path_to_dataset) if
+                         isfile(join(begin_path + path_to_dataset, f))][:local_subset]
 
     train_images = []
 
     for image_name in dataset_filenames:
-        image = Image.open(path_to_dataset + image_name)
+        image = Image.open(begin_path + path_to_dataset + image_name)
         image = np.asarray(image)
         train_images.append(image)
 
     train_images = np.array(train_images)
 
     return train_images
-
