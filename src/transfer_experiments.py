f78519f4d70a4559f13d4ed6dc5b38ad136dbf1d
irmavdbrandt <irma.vdbrandt@gmail.com>
2020-10-14 18:22:48 +0200

    experiment settings for pretraining pcam

--- src/transfer_experiments.py
+++ src/transfer_experiments.py
@@ -54,7 +54,7 @@ def cfg():
     n_folds = None
     img_length = 96
     img_width = 96
-    learning_rate = 0.0001
+    learning_rate = 0.00001
     batch_size = 128
     epochs = 30
     color = True
