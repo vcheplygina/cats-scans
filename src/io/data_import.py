812299506567745c2108d063ba603b17388c23b6
irmavdbrandt <irma.vdbrandt@gmail.com>
2020-10-17 16:11:27 +0200

    experiment settings pretraining sti10

--- src/io/data_import.py
+++ src/io/data_import.py
@@ -209,7 +209,6 @@ def import_STI10(data_dir):
     images = np.load(f'{data_dir}/all_imgs.npy', allow_pickle=True)
     labels = np.load(f'{data_dir}/all_labels.npy', allow_pickle=True)
 
-    all_imgs = [item for sublist in images for item in sublist]
     print(images[0].shape)
 
     # convert labels to integers
@@ -220,8 +219,8 @@ def import_STI10(data_dir):
     print(int_labels)
 
     # split data in train-val-test set (train 80% - val 10% - test 10%)
-    ten_percent = round(0.1 * len(all_imgs))  # define 10% of whole dataset, pass on to split function
-    X_train, X_test, y_train, y_test = train_test_split(all_imgs, int_labels, stratify=int_labels, shuffle=True, random_state=2,
+    ten_percent = round(0.1 * len(images))  # define 10% of whole dataset, pass on to split function
+    X_train, X_test, y_train, y_test = train_test_split(images, int_labels, stratify=int_labels, shuffle=True, random_state=2,
                                                         test_size=ten_percent)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, stratify=y_train, shuffle=True, random_state=2,
                                                       test_size=ten_percent)
