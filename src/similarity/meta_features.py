4d979ed6545631f0fcab7189f97564fa0cc49dc7
BasMulders12 <bas_mulders@hotmail.com>
2020-10-22 16:17:22 +0200

    Removed median and xy_axis features as they did not add anything. Added the line that converts every image to a histogram in order to calculate the statistical values

--- src/similarity/meta_features.py
+++ src/similarity/meta_features.py
@@ -1,44 +1,41 @@
 import numpy as np
 from scipy.stats import kurtosis, skew, entropy as scipy_entropy
+import cv2
 
 #%% Meta-features
 
 # Shannon entropy
 def meta_entropy(image, base=2):
+    image = cv2.calcHist(image, [0], None, [256], [0, 256])
     _, counts = np.unique(image, return_counts=True)
     return scipy_entropy(counts, base=base)
 
 # Skewness
 def meta_skew(image):
+    image = cv2.calcHist(image, [0], None, [256], [0, 256])
     skewness = skew(image, axis=None)
     return skewness
 
 # Kurtosis
 def meta_kurtosis(image):
+    image = cv2.calcHist(image, [0], None, [256], [0, 256])
     kurt = kurtosis(image, axis=None)
     return kurt
 
-# Median
-def meta_median(image):
-    median = np.median(image)
-    return median
-
 # Standard deviation
 def meta_std(image):
+    image = cv2.calcHist(image, [0], None, [256], [0, 256])
     std = np.std(image)
     return std
 
 # Mean
 def meta_mean(image):
+    image = cv2.calcHist(image, [0], None, [256], [0, 256])
     mean = np.mean(image)
     return mean
 
 # Sparsity
 def meta_sparsity(image):
+    image = cv2.calcHist(image, [0], None, [256], [0, 256])
     sparsity = np.count_nonzero(image)/np.prod(image.shape)
     return sparsity
-
-# XY-axis
-def meta_xy_axis(image):
-    xy_axis = (image.shape[0]+image.shape[1])/2
-    return xy_axis
