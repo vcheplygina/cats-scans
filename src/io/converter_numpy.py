9834656a334356ed154ee4e819aa971ec51e165e
BasMulders12 <bas_mulders@hotmail.com>
2020-10-22 16:13:58 +0200

    Removed unused datasets and added the 'None' option to use the whole dataset. Build in a maximum subset of 25000 to avoid memory error and used if-statements to avoid errors if the chosen subset is bigger than the actual size of some datasets. Also converted every image to grayscale and resized to 300 by 300 for consistent calculations.

--- src/io/converter_numpy.py
+++ src/io/converter_numpy.py
@@ -1,8 +1,8 @@
-from PIL import Image
 import numpy as np
 import os
 from os import listdir
 from os.path import isfile, join
+import cv2
 
 #%% Get local images and convert them to numpy arrays
 
@@ -10,12 +10,10 @@ def get_train_images(dataset, converter_path, converter_subset):
     """Retrieve files from local depository and convert them to numpy arrays"""
 
     begin_path = converter_path
-    if dataset == 'ISIC2017':
-        path_to_dataset = '/ISIC2017/'
-    elif dataset == 'ISIC2018':
+    if dataset == 'ISIC2018':
         path_to_dataset = '/ISIC2018/'
     elif dataset == 'chest_xray':
-        path_to_dataset = '/chest_xray/train/'
+        path_to_dataset = '/chest_xray/all/'
     elif dataset == 'stl-10':
         path_to_dataset = '/stl_10/'
     elif dataset == 'dtd':
@@ -27,13 +25,27 @@ def get_train_images(dataset, converter_path, converter_subset):
 
     sub_dirs = next(os.walk(begin_path + path_to_dataset))[1]
     train_images = []
+    names_count = []
 
     for index in range(len(sub_dirs)):
-        dataset_filenames = [f for f in listdir(begin_path + path_to_dataset + sub_dirs[index]) if
-                             isfile(join(begin_path + path_to_dataset + sub_dirs[index], f))][:(round(converter_subset/len(sub_dirs)))]
+        if converter_subset == 'None':
+            converter_subset = 25000        # to avoid memory error
+        else:
+            converter_subset = converter_subset
+
+        all_names = listdir(begin_path + path_to_dataset + sub_dirs[index])
+        for i in range(len(all_names)):
+            names_count.append(all_names[i])
+
+        if len(names_count) < converter_subset:
+            dataset_filenames = [f for f in listdir(begin_path + path_to_dataset + sub_dirs[index]) if
+                                 isfile(join(begin_path + path_to_dataset + sub_dirs[index], f))]
+        else:
+            dataset_filenames = [f for f in listdir(begin_path + path_to_dataset + sub_dirs[index]) if
+                                 isfile(join(begin_path + path_to_dataset + sub_dirs[index], f))][:(round(converter_subset/len(sub_dirs)))]
         for image_name in dataset_filenames:
-            image = Image.open(begin_path + path_to_dataset + sub_dirs[index] + '/' + image_name)
-            image = np.asarray(image)
+            image = cv2.imread(begin_path + path_to_dataset + sub_dirs[index] + '/' + image_name, cv2.IMREAD_GRAYSCALE)
+            image = cv2.resize(image, (300, 300), interpolation=cv2.INTER_CUBIC)
             train_images.append(image)
 
     train_images = np.array(train_images)
