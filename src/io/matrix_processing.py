66b6e24fdcaec112bf5e88084f1443ec758dc2de
BasMulders12 <bas_mulders@hotmail.com>
2020-10-27 00:50:28 +0100

    also inverts values now to make them easier to interpret

--- src/io/matrix_processing.py
+++ src/io/matrix_processing.py
@@ -1,12 +1,19 @@
 #%%
 import numpy as np
 
-def row_normalization(similarity_matrix):
+def norm_and_invert(similarity_matrix):
     max_value = np.max(similarity_matrix)
     new_mat = similarity_matrix/max_value
-    # for row_index in range(len(similarity_matrix)):
-    #     max_row_value = np.max(similarity_matrix[row_index])
-    #     similarity_matrix[row_index] = similarity_matrix[row_index]/max_row_value
-    norm_mat = np.round(new_mat, decimals=2)
+
+    inv_mat = 1/new_mat
+
+    filtered_data = []
+
+    for row in range(inv_mat.shape[0]):
+        for column in range(inv_mat.shape[1]):
+            if row != column:
+                filtered_data.append(inv_mat[row][column])
+
+    norm_mat = np.round(inv_mat/np.max(filtered_data), decimals=2)
 
     return norm_mat
