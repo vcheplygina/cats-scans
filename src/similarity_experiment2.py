f4008d0f7f86a74bbc3ea893b3b6649b3fdf1bdf
Veronika <v.cheplygina@tue.nl>
2021-02-03 10:09:53 +0100

    Added transferability/transfer score

--- src/similarity_experiment2.py
+++ src/similarity_experiment2.py
@@ -11,6 +11,7 @@ import sys
 sys.path.insert(0, join(dirname(__file__), '..'))
 
 from src.io.matrix_processing import norm_and_invert
+from src.io.expert_answer_import import expert_answers
 from src.evaluation.numpy_to_heatmap import make_heatmap
 
 
@@ -19,30 +20,23 @@ from src.evaluation.numpy_to_heatmap import make_heatmap
 data_path = 'outputs'
 
 
-auc = np.load('../outputs/mean_auc_scores.npy')
-auc_dict= pd.read_csv('../outputs/mean_auc_dict.csv')
+#auc = np.load('../outputs/mean_auc_scores.npy')
+#auc_dict= pd.read_csv('../outputs/mean_auc_dict.csv')
+
+absolute_path_local_data = 'C:/Users/VCheplyg/Dropbox/20-lab/cats-scans/cats-scans/data'
+
+
+exp_dist = expert_answers(expert_answer_path=absolute_path_local_data)
+#exp_sim = norm_and_invert(exp_dist) - #Not actually needed for embeddings, distance is what we need
 
 
 datasets_list = ['chest_xray', 'dtd', 'ISIC2018', 'stl-10', 'pcam'] 
-#auc_heatmap = make_heatmap(auc, data_list = datasets_list, output_path = None, auc='yes')
-
-# <--- Dimensions seem to be incorrect, some columns are missing 
-
-exp_sim = [
-        [100,57,71,64,76],
-        [57,100,76,100,71],
-        [71,76,100,64,96],
-        [64,100,64,100,58],
-        [76,71,96,58,100] 
-        ]
-
-exp_dist = np.divide(1, exp_sim) 
 
 
 ############################# MDS
 
 mds = MDS(2, dissimilarity='precomputed', random_state=42, metric=False)
-exp_mds = mds.fit_transform(exp_sim)
+exp_mds = mds.fit_transform(exp_dist)
 
 
 colors=[1,2,3,4,5]
