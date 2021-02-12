91f5a7ba0391e9efa84881446822a85c59b7f0e4
irmavdbrandt <irma.vdbrandt@gmail.com>
2021-02-12 14:37:04 +0100

    add task2vec code

---
+++ src/task2vec_fullcode/PCAM.py
@@ -0,0 +1,46 @@
+from __future__ import print_function, division
+import os
+import torch
+from skimage import io, transform
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from PIL import Image
+from ..io.data_import import collect_data
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class PCAMDataset(Dataset):
+    """PCam dataset."""
+
+    def __init__(self, root_dir, train, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        X_train, X_val, X_test = collect_data(home=root_dir, source_data='pcam-small', target_data=None)
+        if train:
+            self.PCAM = X_train
+        else:
+            self.PCAM = X_test
+        self.root_dir = root_dir
+        self.transform = transform
+        self.targets = self.PCAM['class'].astype(int)
+
+    def __len__(self):
+        return len(self.PCAM)
+
+    def __getitem__(self, idx):
+
+        img_name = self.PCAM.iloc[idx, 0]
+        image = Image.open(img_name)
+        target = int(self.PCAM.iloc[idx, 1])
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, target
---
+++ src/task2vec_fullcode/isic2018.py
@@ -0,0 +1,48 @@
+from __future__ import print_function, division
+import os
+import torch
+from skimage import io, transform
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from PIL import Image
+from ..io.data_import import collect_data
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class ISIC2018Dataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, root_dir, train, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        X_train, X_val, X_test = collect_data(home=root_dir, source_data='isic', target_data=None)
+        if train:
+            self.isic2018 = X_train
+        else:
+            self.isic2018 = X_test
+        # self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.isic2018)
+
+    def __getitem__(self, idx):
+
+        img_name = self.isic2018.iloc[idx, 0]
+        image = Image.open(img_name)
+        target = int(self.isic2018.iloc[idx, 1])
+
+        # label = np.array([label])
+        # labels = label.astype('float').reshape(-1, 2)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, target
