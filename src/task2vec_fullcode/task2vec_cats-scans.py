51a59c0e2bda1404aa3e5c8cf9cf35fd8f5b8165
irmavdbrandt <irma.vdbrandt@gmail.com>
2021-02-23 11:27:46 +0100

    settings sti10

--- src/task2vec_fullcode/task2vec_cats-scans.py
+++ src/task2vec_fullcode/task2vec_cats-scans.py
@@ -15,7 +15,7 @@ def main(cfg: DictConfig):
     train_dataset, test_dataset = get_dataset(cfg.dataset.root, cfg.dataset)
     probe_network = get_model('resnet50', pretrained=True, num_classes=10).cuda()
     embedding = Task2Vec(probe_network).embed(train_dataset)
-    with open(f'{cfg.dataset.root}/embedding_stl10_domain.p', 'wb') as f:
+    with open(f'{cfg.dataset.root}/embedding_sti10_domain.p', 'wb') as f:
     # with open('/Users/IrmavandenBrandt/Downloads/embedding_resnet50.p', 'wb') as f:
         pickle.dump(embedding, f)
 
